a2805242284d4e5f2402d7404ba41c325b715954
GymBosses <57814300+GymBosses@users.noreply.github.com>
2019-12-18 20:08:49 +0300

    F

--- AutoChess/main.cpp
+++ AutoChess/main.cpp
@@ -11,52 +11,50 @@
 #include "Buffs.h"
 ////Size cards = (292, 400)
 
-//something
-
-const int scrX = 3000; //Size (меняет под свой экран, нужно тестировать)
+const int scrX = 3000; //Screen sizes (changes to fit your screen, you need to test)
 const int scrY = 2000;
-bool battle = false;   //Отвечает за режим игры: бой(true) или закупка персонажей
-sf::Vector2f oldPos;   //Перед тем, как передвинуть спрайт, запоминаем его старую позицию
+bool battle = false;   //Responsible for the game mode : fight(true) or purchase of characters
+sf::Vector2f oldPos;   //Before moving the sprite, remember its old position
 
-//задаёт точки(x1, x2...xn, y), где распологаются спрайты(x1,y)(x2,y) магазина и поле игрока
+//sets the points (x1, x2 ... xn, y) where the sprites (x1, y) (x2, y) of the store and the player’s field are located
 void set_position(int* store_postion, float plot, int len);
-bool move_from_bg;     //Берём карточку из баттлграунда(true) или с магазина(false)
+bool move_from_bg;     //We take a card from the battleground (true) or from the store (false)
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(scrX, scrY), "Game 0.001");//рендеринг окна игры
+	sf::RenderWindow window(sf::VideoMode(scrX, scrY), "Game 0.001");//Render Window, where is the game
 	sf::Texture herotexture[15];
 	sf::Sprite heroes[15];
 	//-----------------------------
 	for (int i = 1; i <= 14; i++)
 	{
-		std::string full_path = "image/" + std::to_string(i) + ".jpg"; //путь к файлу jpg
-		herotexture[i - 1].loadFromFile(full_path);					   //загружаем текстуру 
-		heroes[i].setTexture(herotexture[i - 1]);					   //устанавливаем текстуру в спрайт
+		std::string full_path = "image/" + std::to_string(i) + ".jpg"; //file path jpg
+		herotexture[i - 1].loadFromFile(full_path);					   //load texture from file
+		heroes[i].setTexture(herotexture[i - 1]);					   //set texture
 	}
 	//----------------------------------------
 	//------------------------------SET_STORE_POSITION AND PLAYER_FIELD
-	int store_position[5];                 //точки (x1, x2, x3, y), где будут расположены карточки в магазине
+	int store_position[5];                 //points (x1, x2, x3, y) where the cards will be located in the store
 	set_position(store_position, 0.75, 5);
-	int player_field[4];				//аналогично на поле
+	int player_field[4];				//similarly on the field
 	set_position(player_field, 0.50, 4);
 	int comp_field[4];
 	set_position(comp_field, 0.10, 4);
-	//------------------------------ установка бэкграунда
+	//------------------------------ SET BATTLEGROUND AND OTHER IMAGE
 	MySprite background("image/pubg+cats.jpg"); //image background
 	background.full_screen(window.getSize().x, window.getSize().y);
 	background.set_color(255, 255, 255, 128);
 
 	MySprite refresh("image/refresh.png", int(0.05 * scrX), int(0.80 * scrY)); //refresh image
-	MySprite levelup("image/levelup.png", int(0.80 * scrX), int(0.05 * scrY)); 
+	MySprite levelup("image/levelup.png", int(0.80 * scrX), int(0.05 * scrY));
 	MySprite start_game("image/startgame.png", int(0.9 * scrX), int(0.5 * scrY));
 	MySprite died("image/died.jpg");
 	MySprite win("image/win.png", (scrX) / 2 - 200, (scrY) / 2 - 50);
 	MySprite lose("image/lose.png", (scrX) / 2 - 200, (scrY) / 2 - 50);
-
+	//--------------------------------------------------------------------------
 	bool isMove = false;
 	float dx, dy;
-	//-----------------------------Текст с количеством ресурсов у игрока
+	//-----------------------------The text with the amount of resources the player has
 	sf::Font font;
 	font.loadFromFile("font/Alata-Regular.ttf");
 	sf::Text gold;
@@ -70,16 +68,16 @@ int main()
 	gold.setFont(font);
 	gold.setFillColor(sf::Color::White);
 	gold.setCharacterSize(80);
-	gold.setPosition(int(scrX * 0.01), int(scrY * 0.7)); //тут надо подумать, куда поставить
+	gold.setPosition(int(scrX * 0.01), int(scrY * 0.7)); //here you need to think about where to put
 	//------------------------------------------
 	User player;	//Player
 	User temp_player;
 	Computer comp(heroes, comp_field);  //Computer
 	Computer temp_comp = comp;
-	Shop shop(heroes, store_position); //кошачий рынок
+	Shop shop(heroes, store_position); //store
 	Buffs bf;
-	Battleground bg_player(heroes, player_field); //игровое поле
-	gold.setString("Your gold: " + player.get_amount_gold()); //записываем в text строку
+	Battleground bg_player(heroes, player_field); //battlefield
+	gold.setString("Your gold: " + player.get_amount_gold()); //set text to the string
 	sf::Clock clock;
 	bool turn = true;
 	float time = 0;
@@ -87,9 +85,9 @@ int main()
 	while (window.isOpen())
 	{
 		time = clock.getElapsedTime().asSeconds();
-		if (!battle && !shop.store_full)	//если магазин пуст
+		if (!battle && !shop.store_full)	//if store is empty
 		{
-			int max_level = player.num_max_level_heroes(); //уровень таверны
+			int max_level = player.num_max_level_heroes(); //store level
 			shop.set_heroes(max_level);
 		}
 
@@ -98,30 +96,30 @@ int main()
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed) //закрываем окно при нажатии на "Закрыть" (или нет, не знаю)
+			if (event.type == sf::Event::Closed) //close the window when you click on "Close" (or not, I don’t know)
 				window.close();
 
 			if (event.type == sf::Event::MouseButtonPressed && !battle)
 			{
-				if (event.key.code == sf::Mouse::Left) //очевидно
+				if (event.key.code == sf::Mouse::Left) //obviously 
 				{
 					for (int i = 0; i < 4; i++)
 					{
-						if (shop.check_point(i, pos))		//нажали на кого-то из магазина
+						if (shop.check_point(i, pos))		//clicked on someone from the store
 						{
 							isMove = true;
 							move_from_bg = false;
-							shop.set_pl_ch(i);				//номер героя из мазагина, который выбрал игрок
+							shop.set_pl_ch(i);				//the number of the hero from the mazagin that the player chose
 							sf::Vector2f t = shop.get_pos();
 							dx = pos.x - t.x;
 							dy = pos.y - t.y;
 							oldPos = t;
 							break;//брик
 						}
-						else if (i != 3 && bg_player.check_point(i, pos)) //нажали на кого-то из боевого поля
+						else if (i != 3 && bg_player.check_point(i, pos)) //clicked on someone from the battlefield
 						{
 							isMove = true;
-							move_from_bg = true;		//аналогично выше
+							move_from_bg = true;		//similarly higher
 							bg_player.set_pl_ch(i);
 							sf::Vector2f t = bg_player.get_pos();
 							dx = pos.x - t.x;
@@ -133,55 +131,55 @@ int main()
 				}
 			}
 
-			if (event.type == sf::Event::MouseButtonReleased && !battle) //отпускание кнопки мыши
+			if (event.type == sf::Event::MouseButtonReleased && !battle) //releasing the mouse button
 			{
-				if (event.key.code == sf::Mouse::Left)// не правая
+				if (event.key.code == sf::Mouse::Left)// not right
 				{
-					if (isMove) //мы что-то таскали мышкой
+					if (isMove) //we move something
 					{
-						if (!move_from_bg) { //из магазина
+						if (!move_from_bg) { //from store
 							if (!bg_player.check_full_bg() && shop.check_item_on_field(scrY))
-							{//есть ли место на поле и в правильно ли место мы перетащили(0.3*scrY < y < 0.7*scrY)
+							{//is there a place on the field and in the right place have we moved (0.3 * scrY <y <0.7 * scrY)
 								int hero_number;
 								hero_number = shop.get_num_hero();
-								if (player.buy_hero(hero_number)) //можем ли купить, если да - покупаем(нужно разделить на 2 метода)
+								if (player.buy_hero(hero_number)) //can we buy, if yes - buy (need to be divided into 2 methods)
 								{
 									gold.setString("Your gold: " + player.get_amount_gold());
-									shop.delete_item();				//удаляем из магазина
-									bg_player.set_hero(hero_number);//добавляем в игровое поле
+									shop.delete_item();				//delete from the store
+									bg_player.set_hero(hero_number);//add to the playing field
 								}
-								else shop.move_item(oldPos);//если нет денег - возвращаем спрайт
+								else shop.move_item(oldPos);//if there is no gold - return the sprite
 							}
 							else shop.move_item(oldPos);
 						}
-						//Продаём герои из battleground
+						//Sell heroes from battleground
 						else if (move_from_bg && bg_player.check_item_on_field(scrY))
 						{
 							int k = oldPos.x;
-							int index = k / (int(scrX / 4) - 146) - 1; //ууу, математика. какой по счёту(слева направо) спрайт
-							bg_player.sell_hero();		//здесь должен быть delete, но его тут нет
-							player.sell_hero(index);	//продаём по порядковому номеру
+							int index = k / (int(scrX / 4) - 146) - 1; //what is the score (from left to right) sprite (idk)
+							bg_player.sell_hero();
+							player.sell_hero(index);	//sell by serial number
 							gold.setString("Your gold: " + player.get_amount_gold());
 						}
 						else
 						{
-							bg_player.move_item(oldPos); //возвращаем обратно
+							bg_player.move_item(oldPos); //return back
 						}
-						shop.set_pl_ch(-1); //устанавливаем выбор игрока -1 (отстутствует)
+						shop.set_pl_ch(-1); //set player choice -1 (none, empty)
 						bg_player.set_pl_ch(-1);
 						isMove = false;
 					}
 
-					if (refresh.sprite.getGlobalBounds().contains(pos.x, pos.y))//обновление мазазина, завоз новых героев
+					if (refresh.sprite.getGlobalBounds().contains(pos.x, pos.y))//store update, delivery of new heroes
 					{
-						if (player.refresh()) //хватает ли денег(нужно разделить на 2 метода), если да, то -1 голд
+						if (player.refresh()) //is there enough money (need to be divided into 2 methods), if so, then -1 gold
 						{
 							shop.refresh();
 							gold.setString("Your gold: " + player.get_amount_gold());
 						}
 					}
 
-					if (levelup.sprite.getGlobalBounds().contains(pos.x, pos.y)) //Повышение уровня магазина
+					if (levelup.sprite.getGlobalBounds().contains(pos.x, pos.y)) //up level
 					{
 						if (player.up_level())
 						{
@@ -223,7 +221,7 @@ int main()
 		window.clear();
 		window.draw(background.get_sprite());
 		if (!battle)
-		{	//рисовка всех спрайтов, некотыре пустые, возможны ошибки(но их нет)
+		{	//drawing of all sprites, some empty, errors are possible (but they are not)
 			for (int i = 0; i < 3; i++)
 			{
 				window.draw(shop.get_item(i));
@@ -299,7 +297,7 @@ int main()
 			bool check_player = temp_player.all_died();
 			if (check_comp || check_player)
 			{
-				////Жесть, но я не знаю, как иначе///////////////////////////////
+				////its not good, need think about it///////////////////////////////
 				for (int i = 0; i < 3; i++)
 				{
 					if (temp_player.heroes[i].hero_died())
@@ -360,13 +358,13 @@ int main()
 	return 0;
 }
 
-//примите это как неведомую магию(устанавливает координаты на экране в зависимости от разрешения и количества точек
+//sets the coordinates on the screen depending on the resolution and the number of points
 void set_position(int* store_position, float plot, int len)
 {
 	len = len - 1;
 	for (int i = 0; i < len; i++)
 	{
-		store_position[i] = (i + 1) * int(scrX / (len + 1)) - 146;//координаты x
+		store_position[i] = (i + 1) * int(scrX / (len + 1)) - 146;//coordinate x
 	}
-	store_position[len] = int(scrY * plot); //координаты y, она у всех одна(все спрайты на одном уровне моря)
+	store_position[len] = int(scrY * plot); //'y' coordinates, everyone has one 
 }
